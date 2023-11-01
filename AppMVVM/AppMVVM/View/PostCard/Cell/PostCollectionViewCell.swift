a73991e78bbd2aa25eec1d272d76a42e444f5e7b
Leticia Speda <leticiaspeda@gmail.com>
2023-11-01 18:28:49 -0300

    feat: criado a celula customizada PostCollectionViewCell

--- AppMVVM/AppMVVM/View/PostCard/Cell/PostCollectionViewCell.swift
+++ AppMVVM/AppMVVM/View/PostCard/Cell/PostCollectionViewCell.swift
@@ -1,8 +1,29 @@
-//
-//  PostCollectionViewCell.swift
-//  AppMVVM
-//
-//  Created by Leticia Speda on 01/11/23.
-//
+import UIKit
 
-import Foundation
+final class PostCollectionViewCell: UICollectionViewCell, ViewCode {
+    static let identifier = String(describing: PostCollectionViewCell.self)
+    private let screen = PostCollectionViewCellScreen()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) { nil }
+
+    func configureHierarchy() {
+        contentView.addSubview(screen)
+    }
+
+    func configureConstraints() {
+        screen.pin(view: contentView)
+    }
+
+    func setupCell(data: Posts) {  //lista dos posts
+        screen.profileImageView.image = UIImage(named: data.profileImage)
+        screen.userNameLabel.text = data.userName
+        screen.postImageView.image = UIImage(named: data.postImage)
+    }
+
+}
